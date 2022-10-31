d7d0d7313c02fe2bc9ea82211fda880b284b9877
Clement Beaurain <clementbeaurainpro@gmail.com>
2022-10-31 21:17:22 +0100

    10 ok

---
+++ module_00/ex10/loading.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import time
+
+start = time.time()
+
+def getEta(nums, pos):
+  if (pos != 0):
+    avg = (time.time() - start) / pos
+    return round(avg * ((len(nums) - pos)), 2)
+  else:
+    return 'None'
+
+def getTotal(nums, pos):
+  return (f'{pos}/{len(nums)}')
+
+def getElapsed():
+  return (round((time.time() - start), 2))
+
+def printLoading(total, eta, elapsed, percent):
+  if (percent == 0):
+    equals = ''
+  else:
+    equals = '=' * int(percent * 20 / 100)
+  space = ' ' * (20 - len(equals))
+  bar = '[' + equals + space + '>]'
+  print(f'ETA: {eta:.2f}s [{percent: >5}%]{bar} {total} | elapsed time {elapsed:.2f}s', end='\r')
+
+def ft_progress(nums):
+  for elem in nums:
+    pos = nums.index(elem) + 1
+    elapsed = getElapsed()
+    eta = getEta(nums, pos)
+    total = getTotal(nums, pos)
+    percent = pos * 100 / len(nums)
+    printLoading(total, eta, elapsed, percent)
+
+    yield elem
+
+def test():
+  listy = range(1000)
+  ret = 0
+  for elem in ft_progress(listy):
+    ret += (elem + 3) % 5
+    time.sleep(0.01)
+  print()
+  print(ret)
+
+test()
